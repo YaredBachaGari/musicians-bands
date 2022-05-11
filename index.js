134ad70d5c4b95fd665214a9c7a914df6e92f7eb
YaredBG <Yared_Gari@Mckinsey.com>
2022-05-10 22:16:37 -0400

    tested the musician-band sequelize db connection

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+//importing the brand and musician models
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+
+// Relationship between tables
+Musician.belongsTo(Band);
+Band.hasMany(Musician);
+
+
 
 module.exports = {
     Band,
--- index.test.js
+++ index.test.js
@@ -14,11 +14,15 @@ describe('Band and Musician Models', () => {
 
     test('can create a Band', async () => {
         // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const trialBand = await Band.create({name:"Jano", genre: 'rock'})
+        expect(trialBand.name).toBe('Jano');
+        expect(trialBand.genre).toBe('rock');
     })
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
+        const trialMusician = await Musician.create({name:"Abebe", instrument: 'kirar'})
+        expect(trialMusician.name).toBe('Abebe');
+        expect(trialMusician.instrument).toBe('kirar');
     })
 })

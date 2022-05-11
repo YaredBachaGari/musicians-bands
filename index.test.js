0d59f5b89931d8d6c9e8800829df265bf995c306
YaredBG <Yared_Gari@Mckinsey.com>
2022-05-11 01:49:23 -0400

    part2 association test

--- index.test.js
+++ index.test.js
@@ -25,4 +25,19 @@ describe('Band and Musician Models', () => {
         expect(trialMusician.name).toBe('Abebe');
         expect(trialMusician.instrument).toBe('kirar');
     })
+    test('test to check associations Band.hasMany(Musician)', async () => {
+        // TODO - test creating a band
+        const Band1 = await Band.create({name:"Jano", genre: 'rock'})
+        const Band2 = await Band.create({name:"Bano", genre: 'Jazz'})
+        const Musician1 = await Musician.create({name:"Abebe", instrument: 'kirar'})
+        const Musician2 = await Musician.create({name:"Kebed", instrument: 'Masinqo'})
+        const Musician3 = await Musician.create({name:"Bekele", instrument: 'Washint'})
+        await Band1.addMusician(Musician1)
+        await Band1.addMusician(Musician2)
+        await Band1.addMusician(Musician3)
+        const Musicians = await Band1.getMusicians();
+        const bands = await Band.findAll()
+        expect(Musicians.length).toBe(3); 
+        expect(bands.length).toBe(3); 
+    })
 })
